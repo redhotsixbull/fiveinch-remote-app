9e0b4af951356e903b2332173fe18c3e58c85560
redhotsixbull <ccomekdj@gmail.com>
2021-02-01 14:34:29 +0900

    set finger print and local auth

--- android/app/src/main/kotlin/kr/co/five_inch_remote_app/MainActivity.kt
+++ android/app/src/main/kotlin/kr/co/five_inch_remote_app/MainActivity.kt
@@ -1,6 +1,11 @@
 package kr.co.five_inch_remote_app
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
