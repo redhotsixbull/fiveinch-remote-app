a741731525da6fef395534fe83673f4e81f8b854
Sangmin <wtoshm@naver.com>
2021-02-16 17:34:42 +0900

    카테고리 클래스 정의

---
+++ lib/model/category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'massage.dart';
+
+class MassageCategory {
+  String name;
+  Icon icon;
+  List massageList = [];
+
+  MassageCategory({this.name, this.icon, this.massageList});
+}
